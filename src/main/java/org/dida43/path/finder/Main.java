8de89d0709b6fe08c058a26b37d2f4b4b1456948
dida43 <boze.vukman@gmail.com>
2021-10-22 14:37:39 +0200

    remove logging

--- src/main/java/org/dida43/path/finder/Main.java
+++ src/main/java/org/dida43/path/finder/Main.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
 import org.dida43.path.finder.exceptions.AsciiMapException;
 import org.dida43.path.finder.map.AsciiMap;
 import org.dida43.path.finder.map.TravelAsciiMap;
@@ -14,14 +12,6 @@ import org.dida43.path.finder.pojos.Solution;
 public class Main {
 
   public static void main(String[] args) {
-
-    final Logger logger = Logger.getLogger("Software Sauna Code Challenge");
-    try {
-      final FileHandler fileHandler = new FileHandler("asciimap.log");
-    } catch (IOException e) {
-      System.out.println("Cannot write to log file!");
-      System.exit(1);
-    }
 
     if (args.length != 1) {
       System.out.println("You need to specify file path!");
@@ -32,7 +22,6 @@ public class Main {
       mapAsString = readFile(args[0]);
     } catch (IOException e) {
       System.out.println("Cannot read file!");
-      logger.fine(e.getMessage());
       System.exit(1);
     }
 
@@ -42,7 +31,6 @@ public class Main {
       solution = TravelAsciiMap.followPathToSolution(asciiMap);
     } catch (AsciiMapException e) {
       System.out.println("Error");
-      logger.fine(e.getMessage());
       System.exit(1);
     }
 
